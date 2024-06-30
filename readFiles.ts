bbc45a6cad3125be9a6c56c26f9f1ff114f39003
virenn <virenn9999@gmail.com>
2024-06-30 12:58:28 +0530

    obsidian relevant attributes are now inside type Obsidian Card.
    Obsidian Card is now an attribute of node

--- readFiles.ts
+++ readFiles.ts
@@ -1,9 +1,14 @@
 import * as fs from "fs";
 
+type Node = {
+	level: number;
+	text: string;
+};
+
 function classifyHeader(line: string): number {
 	const max_level = 5;
 
-	for (let i = 0; i < max_level; i++) {
+	for (let i = 1; i <= max_level; i++) {
 		if (line.slice(0, i + 1) === "#".repeat(i) + " ") {
 			return i;
 		}
@@ -13,21 +18,37 @@ function classifyHeader(line: string): number {
 
 const filePath = "./test-media/4.Data Engineering.md";
 
-fs.readFile(filePath, "utf-8", (err, data) => {
-	if (err) {
-		console.error("Error reading:", err);
-		return;
-	}
-
-	// console.log('File Contents:',data)
-	console.log(typeof data);
-
-	const lines: Array<string> = data.split("\n");
-
-	console.log(typeof lines);
-	for (const idx in lines) {
-		if (lines[idx][0] == "#") {
-			console.log(classifyHeader(lines[idx]), lines[idx]);
+function loadMarkdown(filePath: string) {
+	const allNodes: Array<Node> = [];
+	fs.readFile(filePath, "utf-8", (err, data) => {
+		if (err) {
+			console.error("Error reading:", err);
+			return;
 		}
-	}
-});
+
+		const lines: Array<string> = data.split("\n");
+
+		let currentNode: Node | null = null;
+
+		for (const line of lines) {
+			const level = classifyHeader(line);
+
+			if (level > -1) {
+				if (currentNode) {
+					allNodes.push(currentNode);
+				}
+				currentNode = { level: level, text: line };
+			} else if (currentNode) {
+				currentNode.text += "\n" + line;
+			}
+		}
+
+		if (currentNode) {
+			allNodes.push(currentNode);
+		}
+
+		console.log(allNodes);
+	});
+}
+
+loadMarkdown(filePath);
