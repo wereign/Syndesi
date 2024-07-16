b423843528782d4c8d2379ab9998a2db60f6d3ef
virenn <virenn9999@gmail.com>
2024-07-16 09:18:16 +0530

    AutoSync

--- main.ts
+++ main.ts
@@ -1,53 +1,91 @@
-import { Plugin } from "obsidian";
+import { Notice, Plugin } from "obsidian";
 import { exec } from "child_process";
 
+interface PluginSettings {
+	maxLevel: number;
+}
+
+const DefaultSettings: Partial<PluginSettings> = {
+	maxLevel: 4,
+};
+
 export default class Syndesi extends Plugin {
-	async onload() {
+	settings: PluginSettings;
+
+	async loadSettings() {
+		this.settings = Object.assign(
+			{},
+			DefaultSettings,
+			await this.loadData()
+		);
+	}
+
+	async saveSettings() {
+		await this.saveData(this.settings);
+	}
+
+	onload() {
 		console.log("Loading Plugin");
+
+		// Register the event to listen for file modifications
+		this.registerEvent(
+			this.app.vault.on("modify", this.onFileModify.bind(this))
+		);
 
 		this.addCommand({
 			id: "create-mindmap",
 			name: "Convert Current Document to Mindmap",
-			callback: () => {
-				console.log(
-					`The current file is ${
-						this.app.workspace.getActiveFile()?.path
-					}`
-				);
-
-				const basePath = (this.app.vault.adapter as any).basePath;
-				const docPath = this.app.workspace.getActiveFile()?.path;
-				const docAbsPath = `${basePath}\\${docPath}`;
-				const canvasPath = docPath?.replace(".md", ".canvas");
-				const canvasAbsPath = `${basePath}\\${canvasPath}`;
-
-				console.log("Document path", docAbsPath);
-				console.log("Canvas path", canvasAbsPath);
-
-				const pythonPath =`${basePath}\\.obsidian\\plugins\\Syndesi\\main.py`;
-				const command = `python "${pythonPath}" --src "${docAbsPath}" --dest "${canvasAbsPath}"`; // Replace with your command
-
-				exec(command, (error, stdout, stderr) => {
-					if (error) {
-						console.error(`Error: ${error.message}`);
-						return;
-					}
-
-					if (stderr) {
-						console.error(`Stderr: ${stderr}`);
-						return;
-					}
-
-					console.log(`Stdout: ${stdout}`);
-
-				});
-
-				console.log("End")
-			},
+			callback: this.convertToMindmap.bind(this),
 		});
 	}
 
-	async onunload() {
+	onFileModify(file) {
+		const activeFile = this.app.workspace.getActiveFile();
+		if (file.path === activeFile?.path){
+			console.log(`File modified: ${file.path}`);
+			this.convertToMindmap();
+			new Notice('Updated Mindmap')
+
+		}
+	}
+
+	convertToMindmap() {
+		const activeFile = this.app.workspace.getActiveFile();
+		if (!activeFile) {
+			console.log("No active file found.");
+			return;
+		}
+
+		const basePath = (this.app.vault.adapter as any).basePath;
+		const docPath = activeFile.path;
+		const docAbsPath = `${basePath}\\${docPath}`;
+		const canvasPath = docPath.replace(".md", ".canvas");
+		const canvasAbsPath = `${basePath}\\${canvasPath}`;
+
+		console.log("Document path", docAbsPath);
+		console.log("Canvas path", canvasAbsPath);
+
+		const pythonPath = `${basePath}\\.obsidian\\plugins\\Syndesi\\main.py`;
+		const command = `python "${pythonPath}" --src "${docAbsPath}" --dest "${canvasAbsPath}"`;
+
+		exec(command, (error, stdout, stderr) => {
+			if (error) {
+				console.error(`Error: ${error.message}`);
+				return;
+			}
+
+			if (stderr) {
+				console.error(`Stderr: ${stderr}`);
+				return;
+			}
+
+			console.log(`Stdout: ${stdout}`);
+		});
+
+		console.log("End");
+	}
+
+	onunload() {
 		console.log("Unloading Plugin");
 	}
 }
---
+++ settings.ts
@@ -0,0 +1,23 @@
+// import { text } from "d3";
+// import Syndesi from "main";
+// import {App,Plugin,PluginSettingTab,Setting} from "obsidian"
+
+
+// export class SyndesiSettings extends PluginSettingTab {
+//     plugin: Syndesi
+
+//     constructor(app:App,plugin:Syndesi) {
+//         super(app,plugin)
+//         this.plugin = plugin;
+//     }
+
+//     display(): void {
+//         const {containerEl} = this;
+        
+
+//         containerEl.empty()
+
+//         new Setting(containerEl).setName('Max Header Level').setDesc('Fill later').addText((text) =>
+//         text.setPlaceholder()) 
+//     }
+// }
